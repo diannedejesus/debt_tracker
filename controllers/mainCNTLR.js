6bee147c0b297f6188423da054a5c498786e4405
Dianne <djs.dianne@gmail.com>
2022-07-18 17:43:56 -0400

    modified ejs and controller template

--- controllers/mainCNTLR.js
+++ controllers/mainCNTLR.js
@@ -1,26 +1,31 @@
 //you can use this section to declare dependancies that any of your functions can use.
 
 module.exports = { //create a module that will be readable to any code that calls it. In other words when you access this module you will be able to use the individual functions declare in it with dot notation
-    index: async (req,res)=>{ //defining a variable (property or method) with the name index, it with be an asychronous function as defined by the async keyword and the arrow function expression. You could just as easily written: async function(req,res){}
-        try{
-            res.render('index.ejs', { 
+    index: async (req,res)=>{ //defining a method with the name index, it with be an asychronous function as defined by the async keyword and the arrow function expression. 
+        try{ //when working with async we are working with outside requests which can fail for various reasons. The try/catch pair with run the code and if a request fails it will defaults to catch.
+            res.render('index.ejs', { //res as in respond, so we are responding by invoking the render method and telling it to render the page (interprete the code and show the resulting html) index.ejs. We can also pass it an object with key/value pairs for the page to use.
                 //user: user,
             })
         }catch(err){
-            console.log(err)
+            console.log(err) //display the error in the console
         }
     },
     
-    // letterView: async (req,res)=>{
-    //     try{
-    //         const info = await HistoricImportDB.findOne({accessLink: req.params.idCode})
+    useAsyncFunction: async function(req,res){ //defining a method with the name useAsyncFunction, it with be an asychronous function as defined by the async and function keyword. 
+        try{//when working with async we are working with outside requests which can fail for various reasons. The try/catch pair with run the code and if a request fails it will defaults to catch.
+            res.render('index.ejs', { //res as in respond, so we are responding by invoking the render method and telling it to render the page (interprete the code and show the resulting html) index.ejs. We can also pass it an object with key/value pairs for the page to use.
+                //user: user,
+            })
+        }catch(err){
+            console.log(err) //display the error in the console
+        }
+    },
 
-    //         res.render('letter.ejs', { info, messages: req.query.messages })
-
-    //     }catch(err){
-    //         console.log(err)
-    //     }
-    // },
+    useFunction: function(req,res){ //defining a method with the name useFunction, it with be a function as defined by function keyword or could be rewitten as an arrow key function. 
+        res.render('index.ejs', { //res as in respond, so we are responding by invoking the render method and telling it to render the page (interprete the code and show the resulting html) index.ejs. We can also pass it an object with key/value pairs for the page to use.
+            //user: user,
+        })
+    },
 
     
 
--- routes/mainRTE.js
+++ routes/mainRTE.js
@@ -5,4 +5,9 @@ const mainController = require('../controllers/mainCNTLR') //path to the control
 
 router.get('/', mainController.index) //define a url and which code to execute for that url. In this example if the url / is accessed (the root/default page) then execute the function index from the file that is stored in mainController
 
+router.get('/asyncFunc', mainController.useAsyncFunction) //define a url and which code to execute for that url. In this example if the url /asyncFunc is accessed then execute the function useAsyncFunction from the file that is stored in mainController
+router.get('/func', mainController.useFunction) //define a url and which code to execute for that url. In this example if the url /func is accessed then execute the function useFunction from the file that is stored in mainController
+
+
+
 module.exports = router
